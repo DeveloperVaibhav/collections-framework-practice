08da810df1e94ad97ccaaceacc5bd48bab123c02
Vaibhav <vaibhavchafle09@gmail.com>
2026-01-03 23:59:48 +0530

    Operations on list

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,38 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+
+        List<String> fruitList = new ArrayList<>();
+        fruitList.add("Apple");
+        fruitList.add("Banana");
+        fruitList.add("Cherry");
+        fruitList.add("Date");
+        fruitList.add("Elderberry");
+
+        System.out.println("Fruit list: " + fruitList);
+
+        //create a list of employees
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(1, "Alice", "HR", 60000));
+        employees.add(new Employee(2, "Ethan", "IT", 75000));
+        employees.add(new Employee(3, "Charlie", "Finance", 80000));
+        employees.add(new Employee(4, "Jack", "IT", 72000));
+        employees.add(new Employee(5, "Ethan", "HR", 58000));
+
+        //sort the employees by name and if name is same then sort by department
+        Comparator<Employee> employeeComparator = Comparator.comparing(Employee::getName)
+                .thenComparing(Employee::getDepartment);
+        employees.sort(employeeComparator);
+
+        //System.out.println("Sorted Employees:" + employees);
+        System.out.println("-----------------------------------");
+        System.out.println("Id | Name | Department | Salary");
+        System.out.println("-----------------------------------");
+        //print the sorted employees
+        for(Employee emp : employees){
+            System.out.println(emp);
+        }
 
     }
 }
